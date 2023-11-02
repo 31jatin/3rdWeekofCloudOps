e972ddc8b76a3b4653ed375ad383360bd415cd9e
Jatin Vaghela <38216176+31jatin@users.noreply.github.com>
2023-11-02 13:41:37 +0530

    Update backend.tf

--- book_shop_app/backend.tf
+++ book_shop_app/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "learning-terraform"
+    bucket = "learning-t"
     key    = "backend/books_shop_app.tfstate"
     region = "us-east-1"
     dynamodb_table = "dynamoDB-state-locking"
